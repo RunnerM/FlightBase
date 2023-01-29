5e01255dcf1d16570944eee752e0fa2677c945dc
Marton Pentek <pentek.marci@gmail.com>
2023-01-29 21:30:05 +0100

    Initial commit

---
+++ FlightBase/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightBase;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ FlightBase/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightBase;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
