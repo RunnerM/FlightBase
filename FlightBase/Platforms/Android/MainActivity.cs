c960284fc342bdd5be4e670f6551d5ff386cdd10
Marton Pentek <pentek.marci@gmail.com>
2023-02-04 20:22:24 +0100

    add android serial driver

--- FlightBase/Platforms/Android/MainActivity.cs
+++ FlightBase/Platforms/Android/MainActivity.cs
@@ -1,12 +1,25 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Hardware.Usb;
 using Android.Content.PM;
-using Android.OS;
+using Hoho.Android.UsbSerial.Driver;
+
+[assembly: UsesFeature("android.hardware.usb.host")]
 
 namespace FlightBase;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[IntentFilter(new[] { UsbManager.ActionUsbDeviceAttached })]
+[MetaData(UsbManager.ActionUsbDeviceAttached, Resource = "@xml/device_filter")]
 public class MainActivity : MauiAppCompatActivity
 {
+    static readonly string TAG = typeof(MainActivity).Name;
+    const string ACTION_USB_PERMISSION = "com.hoho.android.usbserial.examples.USB_PERMISSION";
+
+    UsbManager usbManager;
+    BroadcastReceiver detachedReceiver;
+    UsbSerialPort selectedPort;
+    
 }
