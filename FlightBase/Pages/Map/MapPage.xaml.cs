0f2b2bdb0301e13157e945be2244d5c8ba7b3a74
Marton Pentek <pentek.marci@gmail.com>
2023-02-12 11:12:37 +0100

    swapp to mapview

--- FlightBase/Pages/Map/MapPage.xaml.cs
+++ FlightBase/Pages/Map/MapPage.xaml.cs
@@ -1,56 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FlightBase.Shared.Domain.Model;
-using FlightBase.Shared.ViewModel;
-using Mapsui;
+﻿using FlightBase.Shared.ViewModel;
 using Mapsui.Extensions;
-using Mapsui.Layers;
-using Mapsui.Nts;
-using Mapsui.Nts.Extensions;
-using Mapsui.Nts.Providers;
-using Mapsui.Providers;
 using Mapsui.Tiling;
-using Mapsui.Widgets;
-using Mapsui.Widgets.ScaleBar;
 using Mapsui.Widgets.Zoom;
-using Microsoft.Maui.Controls.Shapes;
-using Microsoft.Maui.Maps;
-using NetTopologySuite.Features;
 using NetTopologySuite.Geometries;
 using Color = Mapsui.Styles.Color;
-using Easing = Mapsui.Utilities.Easing;
-using Location = Microsoft.Maui.Devices.Sensors.Location;
+using Position = Mapsui.UI.Maui.Position;
+
 
 namespace FlightBase;
 
 public partial class MapPage : ContentPage
 {
-
     public MapPage(MapViewModel mapViewModel)
     {
         InitializeComponent();
         //Location location;
         //location = Geolocation.GetLastKnownLocationAsync().Result;
-        
+
         //if (location != null)
-           // Map.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(1)));
+        // Map.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(1)));
         //var mapControl = new Mapsui.UI.Maui.MapControl();
-        var map = MapControl.Map;
-        map?.Layers.Add(OpenStreetMap.CreateTileLayer());
-        var widget = new ZoomInOutWidget
+        MapView.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
+        MapView.Pins.Add(new Mapsui.UI.Maui.Pin()
         {
-            BackColor = Color.Black,
-            StrokeColor = Color.White,
-            MarginY = 20,
-            MarginX = 10
-        };
-        map?.Widgets.Add(widget);
+            Label = "Test",
+            Position = new Position(55.8581, 9.8476)
+        });
+        MapView.Drawables.Add(new Mapsui.UI.Maui.Polyline()
+        {
+            Positions =
+            {
+                new Position(55.8581, 9.8476),
+                new Position(55.8571, 9.8466),
+                new Position(55.8561, 9.8456),
+            },
+            StrokeColor = Colors.Red,
+            StrokeWidth = 5
+        });
 
 
         //BindingContext = mapViewModel;
     }
-    
 }
